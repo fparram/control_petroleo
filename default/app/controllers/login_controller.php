5d16dcdf0d3bb1acb1973e470dfc8b6b28fd3a88
fparram <39342409+fparram@users.noreply.github.com>
2018-05-16 18:42:00 -0400

    Update login_controller.php

--- default/app/controllers/login_controller.php
+++ default/app/controllers/login_controller.php
@@ -9,10 +9,10 @@ Load::model('users');
 class LoginController extends AppController {
 
     public function index() {
-        if (Input::hasPost("email", "pass")) {
-            $pwd = Input::post("pass");
+        if (Input::hasPost("email", "password")) {
+            $pwd = Input::post("password");
             $usuario = Input::post("email");
-            $auth = new Auth("model", "class: users", "email: $usuario", "pass: $pwd");
+            $auth = new Auth("model", "class: users", "email: $usuario", "password: ".  Util::encriptarClave($pwd));
             if ($auth->authenticate()) {               
                 Redirect::to("/");                
             } else {
